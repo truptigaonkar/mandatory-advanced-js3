d4df68c022b69fef366d5154e5276bb7e3055659
truptigaonkar <trups11.trupti@gmail.com>
2019-04-05 14:31:28 +0200

    Fetching todo list successfully

--- src/components/todos.js
+++ src/components/todos.js
@@ -14,19 +14,36 @@ export default class Todos extends Component {
       token: token$.value,
       email: '',
       message: '',
-      newtodo: ''
+      newtodo: '',
+      todolist: []
     }
   }
 
-  // Getting profile (email) information
   componentDidMount() {
+    // Getting profile (email) information
     if (!!this.state.token) {
       const decoded = jwt.decode(token$.value);
-      console.log("Decoded data: ", decoded);
+      console.log("Decoded email: ", decoded.email);
       this.setState({ email: decoded.email })
     } else {
       this.props.history.push("/login");
     }
+
+    // Fetching todo list
+    axios
+      .get(API_ROOT + "/todos",
+        {
+          headers: { Authorization: "Bearer " + token$.value, }
+        })
+      .then(response => {
+        console.log("Response is: ", response);
+        this.setState({ todolist: response.data.todos })
+        //console.log("Todo list contains: ", this.state.todolist[0]);
+      })
+      .catch(error => {
+        console.log(error);
+        this.setState({ message: "Something went wrong !!!!" });
+      })
   }
 
   // Necessary step: when you perform componentDidMount(), it is advisable to use componentWillUnmount()
@@ -37,7 +54,7 @@ export default class Todos extends Component {
   // Handling logout button
   handleLogout(e) {
     e.preventDefault();
-    updateToken(null); // Removing token
+    updateToken(null);
     this.props.history.push("/login");
   }
 
@@ -51,11 +68,13 @@ export default class Todos extends Component {
   handleFormSubmit(e) {
     e.preventDefault();
     axios
-      .post(API_ROOT + "/todos", 
+      .post(API_ROOT + "/todos",
         { content: this.state.newtodo },
         { headers: { Authorization: "Bearer " + token$.value } }
       )
       .then((response) => {
+        console.log(response);
+        this.componentDidMount() // This helps to see todo list added after adding 
         this.setState({
           message: "Created new todo successfully",
           newtodo: "", // Clearing an input value after form submit
@@ -67,7 +86,6 @@ export default class Todos extends Component {
   }
 
   render() {
-    console.log(this.state);
     return (
       <>
         <Helmet>
@@ -76,12 +94,27 @@ export default class Todos extends Component {
 
         <p>Welcome User, {this.state.email} <button type="button" onClick={this.handleLogout.bind(this)}>todos.js Logout</button></p>
 
-        <h2>Todo list</h2>
+        <h2>Todo Page</h2>
         <div className="message">{this.state.message}</div> {/* erroror messsage */}
+
+        {/* Creating new todos */}
         <form onSubmit={this.handleFormSubmit.bind(this)}>
           <input type="text" value={this.state.newtodo} onChange={this.handleChange.bind(this)} />
           <button>Add Todos</button>
-        </form>
+        </form><br />
+
+        {/* Listing todos */}
+        {
+          this.state.todolist.map(todo => {
+            console.log("Single todo is: ", todo);
+            return (
+              <ul key={todo.id}>
+                <li >{todo.content}</li>
+              </ul>
+            )
+          })
+        }
+
 
       </>
     )
