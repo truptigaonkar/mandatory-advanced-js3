7f631ba395e60211fd5df795ad03ecf8a4e858e0
truptigaonkar <trups11.trupti@gmail.com>
2019-04-27 13:51:07 +0200

    Updates

---
+++ precache-manifest.be261229a1b5c893624a28b043e914d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b71de15a92e10a87b218ae3299a477bc",
+    "url": "/mandatory-advanced-js3/static/media/image2.b71de15a.jpg"
+  },
+  {
+    "revision": "4eb282d2593262c93020",
+    "url": "/mandatory-advanced-js3/static/js/runtime~main.2924bc53.js"
+  },
+  {
+    "revision": "fcaaa08a43580161f38a",
+    "url": "/mandatory-advanced-js3/static/js/main.ecc026de.chunk.js"
+  },
+  {
+    "revision": "802d512475470392dd0a",
+    "url": "/mandatory-advanced-js3/static/js/2.329de2e9.chunk.js"
+  },
+  {
+    "revision": "fcaaa08a43580161f38a",
+    "url": "/mandatory-advanced-js3/static/css/main.4b4892aa.chunk.css"
+  },
+  {
+    "revision": "28f2b0969b5cce2a7cd10f4802e9e2fb",
+    "url": "/mandatory-advanced-js3/index.html"
+  }
+];
